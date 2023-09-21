35849275e896489317d14674a4d44a1b50758337
kaushik-godhani <68903480+kaushik-godhani@users.noreply.github.com>
2023-09-21 18:58:29 +0530

    security changes

--- src/Firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDCcQ1bQmv5MgYgUutREOEcnLR-SWqymw0",
-    authDomain: "whatsapp-clone-ef9ea.firebaseapp.com",
-    projectId: "whatsapp-clone-ef9ea",
-    storageBucket: "whatsapp-clone-ef9ea.appspot.com",
-    messagingSenderId: "660304199238",
-    appId: "1:660304199238:web:d7d5afa9bb5b4f2e7e5447"
-  };
